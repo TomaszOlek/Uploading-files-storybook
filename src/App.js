3b30839a3a7a411eebd6b80e559c7d3671a9ec04
TomaszOlek <tomasz.olek.dev@gmail.com>
2022-12-07 13:21:30 +0100

    Basic setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: calc(100vw - 300px);
+  height: 100vh;
+`
+const ContentContainer = styled.div`
+  width: 100%;
+  height: calc(100vh - 80px);
+  background-color: white;
+`
+const NavigationBar = styled.div`
+  width: 300px;
+  height: 100vh;
+  background-color: black;
+`
+const ProfileBar = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: gray;
+`
+
+function AllFiles() {
+  return (
+    <ContentContainer>
+      123
+    </ContentContainer>
+  );
+}
+
+function Home() {
+  return (
+    <ContentContainer>
+      456
+    </ContentContainer>
+  );
+}
+
+function Profile() {
+  return (
+    <ProfileBar>
+
+    </ProfileBar>
+  );
+}
+
+function NavBar() {
+  return (
+    <NavigationBar>
+
+    </NavigationBar>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      Test
+    <div style={{display:"flex", flexDirection:"row"}}>
+      <NavBar/>
+      <Content>
+        <Profile/>
+        {true ? //later check which tab is opened insted
+        <Home/>
+        :
+        <AllFiles/>
+        }
+      </Content>
     </div>
   );
 }
--- src/stories/App.stories.js
+++ src/stories/App.stories.js
@@ -1,11 +1,16 @@
 import React from "react";
 import App from "../App";
 
-export default { title: "App" };
+export default { 
+  title: 'App',
+  parameters: {
+    layout: 'fullscreen'
+  },
+};
 
 export const app = () => {
   return <App data={{}} />;
 };
 app.story = {
-  name: "MySleep",
+  name: 'App prototype',
 };
