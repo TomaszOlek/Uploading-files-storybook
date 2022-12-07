9cd1f879843288b9e4e861b7e6054e694e3afc9b
TomaszOlek <tomasz.olek.dev@gmail.com>
2022-12-07 12:16:52 +0100

    first commit

---
+++ src/stories/App.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "../App";
+
+export default { title: "App" };
+
+export const app = () => {
+  return <App data={{}} />;
+};
+app.story = {
+  name: "MySleep",
+};
